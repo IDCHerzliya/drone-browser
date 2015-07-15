deef8b7555b6ef1a52099c22bea75edd8c1f2cdf
Kevin Zhai <kevzhai@users.noreply.github.com>
2015-07-15 16:12:57 -0700

    trying to figure out distract stops

--- public/client.js
+++ public/client.js
@@ -1,14 +1,24 @@
 (function() {
-  var faye, keymap, speed, firstMove, directions, hz;
+  var faye, keymap, speed, firstMove, directions, hz, distract, moveCounter;
   var LED_KEY = 89;
   var MAX_SPEED = parseFloat($("#speed").val());
   var CONCENTRATION_LEVEL = parseFloat($("#concentration").val());
+  var RANDOM_MIN = 1; // in seconds
+  var RANDOM_MAX = 0.5; 
+  var MAX_MOVE = 10; // counter to prevent queueing of commands from sleep()
   function reset() {
     speed = 0;
-    firstMove = true; 
     return faye.publish("/drone/drone", {
       action: 'stop'
     });
+  }
+  function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
   }
 
   faye = new Faye.Client("/faye", {
@@ -38,47 +48,47 @@
     });
   });
   keymap = {
-    87: {
+    87: { //'w'
       ev: 'move',
       action: 'front'
     },
-    83: {
+    83: { //'s' 
       ev: 'move',
       action: 'back'
     },
-    65: {
+    65: { //'a'
       ev: 'move',
       action: 'left'
     },
-    68: {
+    68: { //'d'
       ev: 'move',
       action: 'right'
     },
-    38: {
+    38: { //'up'
       ev: 'move',
       action: 'up'
     },
-    40: {
+    40: { //'down'
       ev: 'move',
       action: 'down'
     },
-    37: {
+    37: { //'left'
       ev: 'move',
       action: 'counterClockwise'
     },
-    39: {
+    39: { //'right'
       ev: 'move',
       action: 'clockwise'
     },
-    32: {
+    32: { //'space'
       ev: 'drone',
       action: 'takeoff'
     },
-    27: {
+    27: { //'esc'
       ev: 'drone',
       action: 'land'
     },
-    84: {
+    84: { //'t'
       ev: 'animate',
       action: 'turnaround',
       duration: 15
@@ -86,30 +96,30 @@
     112: {
       ev: 'animate',
       action: 'theta30Deg', //forward
-      duration: 15
+      duration: 30
     },
     113: {
       ev: 'animate',
       action: 'thetaM30Deg', //backward
-      duration: 15
+      duration: 30
     },
     114: {
       ev: 'animate',
       action: 'phi30Deg',
-      duration: 50
+      duration: 30
     },
     115: {
       ev: 'animate',
       action: 'phiM30Deg',
-      duration: 50
+      duration: 30
     },
-    89: {
+    89: { //'y'
       ev: 'animateLeds',
       action: 'blinkGreenRed',
       hz: 5,
       duration: 3
     },
-    69: {
+    69: { //'e'
       ev: 'drone',
       action: 'disableEmergency'
     }
@@ -133,6 +143,8 @@
 
   speed = 0;
   firstMove = true; 
+  distract = false;
+  moveCounter = 0;
   $(document).keydown(function(ev) {
     var evData;
     MAX_SPEED = parseFloat($("#speed").val());
@@ -142,33 +154,48 @@
     }       
     ev.preventDefault();
     speed = speed >= MAX_SPEED ? MAX_SPEED : speed + 0.08 / (1 - speed);
-    if (firstMove && ev.keyCode in directions) {
-      firstMove = false;
-      evData = keymap[directions[ev.keyCode]];
+    
+    if (moveCounter < MAX_MOVE) {
+      moveCounter++; // do we need moveCounter?
+      if (distract) {
+        var milliseconds = Math.floor(Math.random() * (RANDOM_MAX - RANDOM_MIN + 1) + RANDOM_MIN);
+        sleep(milliseconds * 1000);
+        distract = false;
+        reset();
+      }
+      if (firstMove && ev.keyCode in directions) {
+        firstMove = false;
+        evData = keymap[directions[ev.keyCode]];
+        return faye.publish("/drone/" + evData.ev, {
+          action: evData.action,
+          speed: speed,
+          duration: evData.duration
+        });
+      }  
+      evData = keymap[ev.keyCode];
+      if (ev.keyCode == LED_KEY) {
+        return faye.publish("/drone/" + evData.ev, {
+          action: evData.action,
+          hz: evData.hz,
+          duration: evData.duration
+        });
+      };
+      if (Math.random() > CONCENTRATION_LEVEL) { 
+        distract = true;
+        reset();
+      };
       return faye.publish("/drone/" + evData.ev, {
         action: evData.action,
         speed: speed,
         duration: evData.duration
-      });
-    }  
-    evData = keymap[ev.keyCode];
-    if (ev.keyCode == LED_KEY) {
-      return faye.publish("/drone/" + evData.ev, {
-        action: evData.action,
-        hz: evData.hz,
-        duration: evData.duration
-      });
-    };
-    if (Math.random() > CONCENTRATION_LEVEL) { 
+      }); 
+    } else {
+      moveCounter = 0;
       reset();
     };
-    return faye.publish("/drone/" + evData.ev, {
-      action: evData.action,
-      speed: speed,
-      duration: evData.duration
-    });
   });
   $(document).keyup(function(ev) {
+    firstMove = true; 
     reset();
   });
   $("*[data-action]").on("mousedown", function(ev) {
